3a4c1ca509506712a2d8ba5343ac8e8f08bcc9b5
tetrisTESTER22 <niepielievdaniil@gmail.com>
2025-05-15 08:54:37 +0300

    small edits

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # порт React
+    allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
